9669cc848a4c86b52924b8033b3f0e37ce093971
John Rocha <xjohnfitcodes@gmail.com>
2026-06-19 12:34:00 -0400

    feat: implement login and registration forms with input handling and error messaging

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,42 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { Link } from 'react-router';
+import "../styles/register.css";
+import { useState } from 'react';
 
-export default Login
+const Login = () => {
+
+    const [userData, setUserData] = useState(
+        {
+            email: '',
+            password: '',
+        }
+    );
+
+    // Function to change controlled input
+    const inputHandler = (e) => {
+        setUserData((prevState) => {
+            return { ...prevState, [e.target.name]: e.target.value}
+        });
+    }
+
+    return (
+        <section className="register">
+            <div className="container register__container">
+                <h2>Login</h2>
+                <form>
+                    <p className="form__error-message">
+                        Error from the backend
+                    </p>
+                    <input onChange={inputHandler} type="email" name="email" placeholder="Email Address" autoComplete="true" />
+                    <input onChange={inputHandler} type="password" name="password" placeholder="Password" autoComplete="true" />
+                    <p>
+                        Don't have an account?
+                        <Link to='/register'>Register Now</Link>
+                    </p>
+                    <button type='submit' className="btn primary">Login</button>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,48 @@
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+import { Link } from 'react-router';
+import "../styles/register.css";
+import { useState } from 'react';
 
-export default Register
+const Register = () => {
+
+    const [userData, setUserData] = useState(
+        {
+            fullName: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+        }
+    );
+
+    // Function to change controlled input
+    const inputHandler = (e) => {
+        setUserData((prevState) => {
+            return { ...prevState, [e.target.name]: e.target.value}
+        });
+    }
+
+    console.log(userData)
+
+    return (
+        <section className="register">
+            <div className="container register__container">
+                <h2>Register</h2>
+                <form>
+                    <p className="form__error-message">
+                        Error from the backend
+                    </p>
+                    <input onChange={inputHandler} type="text" name="fullName" placeholder="Full Name" autoComplete="true" autoFocus />
+                    <input onChange={inputHandler} type="email" name="email" placeholder="Email Address" autoComplete="true" />
+                    <input onChange={inputHandler} type="password" name="password" placeholder="Password" autoComplete="true" />
+                    <input onChange={inputHandler} type="password" name="confirm-password" placeholder="Confirm Password" autoComplete="true" />
+                    <p>
+                        Already have an account?
+                        <Link to='/'> Sign In</Link>
+                    </p>
+                    <button type='submit' className="btn primary">Register</button>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Register;
